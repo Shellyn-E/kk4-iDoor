b2ea1919461f5c7a862edaf1045771d92edaeaa5
Shellyn Euriska <lycorisilytheayeh@gmail.com>
2023-01-08 15:41:46 +0700

    first commit

---
+++ app/src/main/java/com/shellyn/idoor/ActivityDua.kt
@@ -0,0 +1,11 @@
+package com.shellyn.idoor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityDua : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dua)
+    }
+}
---
+++ app/src/main/java/com/shellyn/idoor/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.shellyn.idoor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLoginListener()
+    }
+
+    private fun btnLoginListener() {
+        button.setOnClickListener {
+            startActivity(Intent(this, ActivityDua::class.java))
+        }
+    }
+}
